d08af24ccc53e53e3173c3dcf13e85e904923927
Brian <brian.m.muigai@gmail.com>
2024-04-03 11:32:11 +0300

    feat:completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const removeMain=document.getElementById('main')
+removeMain.remove(main);
+const newHeader=document.createElement('h1')
+newHeader.id="victory"
+newHeader.textContent="Brian is the champion"
